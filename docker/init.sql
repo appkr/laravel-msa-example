6ab4a48d21234562cd6469b34f221fba6645225a
appkr <juwonkim@me.com>
2021-02-23 20:09:30 +0900

    fix typo in mysql init script

--- docker/init.sql
+++ docker/init.sql
@@ -1,1 +1,1 @@
-create database if not exists `local_example default character set = utf8 default collate = utf8_general_ci;
+create database if not exists `local_example` default character set = utf8 default collate = utf8_general_ci;
